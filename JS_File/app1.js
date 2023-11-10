b6ea10a709b3449dbeeabc9a061e887b41aa0620
chocody <108443531+chocody@users.noreply.github.com>
2023-11-10 22:52:09 +0700

    edit

--- JS_File/app1.js
+++ JS_File/app1.js
@@ -1,25 +1,37 @@
 const express =require('express');
 const mysql = require('mysql2');
-const router = express.Router();
 const app = express();
 const dotenv = require('dotenv')
-const PORT = 3030;
 dotenv.config();
 
-// let connection = mysql.createConnection({
-//     host : process.env.MYSQL_HOST,
-//     user : process.env.MYSQL_USERNAME,
-//     password : process.env.MYSQL_PASSWORD,
-//     database : process.env.MYSQL_DATABASE
-// });
+const router = express.Router();
+app.use(router);
+
+let connection = mysql.createConnection({
+    host : process.env.MYSQL_HOST,
+    user : process.env.MYSQL_USERNAME,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE
+});
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
-// connection.connect((err){
-//     if(err) throw err;
-//     console.log(`Connected DB : ${process.env.MYSQL_DATABASE}`);
-// })
+connection.connect((err) =>{
+    if(err) throw err;
+    console.log(`Connected DB : ${process.env.MYSQL_DATABASE}`);
+})
 
-app.listen(PORT, ()=>{
+router.get('/tools', (req, res) => {
+    console.log("Requested at: "+req.url);
+    let sql = `SELECT * FROM topkingdb`;
+    connection.query( sql, function (error, results) {
+        res.send(results)
+    });
+})
+
+app.listen(process.env.MYSQL_PORT, ()=>{
     console.log(`Server is listening to Port: ${PORT}`);
 });
