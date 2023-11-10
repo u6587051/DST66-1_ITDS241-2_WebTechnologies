30279bea7c25ec43e7250d79fb570cf97385652d
thanaguide <thanawich.jue@student.mahidol.edu>
2023-11-10 22:38:06 +0700

    set app1

--- JS_File/app1.js
+++ JS_File/app1.js
@@ -1,5 +1,20 @@
 const express =require('express');
 const mysql = require('mysql2');
-const app = express();
 const router = express.Router();
+const app = express();
+const mysql = require('mysql2')
+const dotenv = require('dotenv')
+dotenv.config();
+
+let connection = mysql.createConnection({
+    host : process.env.MYSQL_HOST,
+    user : process.env.MYSQL_USERNAME,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE
+});
+
 app.use(router);
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening to Port: ${PORT}`);
+})
