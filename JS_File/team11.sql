9fb03b436fee9fdabb025502ccb032bd1a7c8d7c
chocody <108443531+chocody@users.noreply.github.com>
2023-11-13 22:24:28 +0700

    sql and html edit

--- JS_File/team11.sql
+++ JS_File/team11.sql
@@ -5,6 +5,8 @@ USE team11;
 CREATE TABLE Product (
 PID char(2) NOT NULL UNIQUE,
 PName varchar(100) NOT NULL,
+PBrand varchar(40) NOT NULL,
+PCat varchar(50) NOT NULL,
 PPrice int NOT NULL,
 PQuan int NOT NULL,
 PDetail varchar(256) NOT NULL,
@@ -36,12 +38,12 @@ INSERT INTO USERS VALUES
 ("105","pipat4@gmail.com","99994",NULL,NULL,NULL,NULL,NULL,0);
 
 INSERT INTO Product VALUES
-("01","ใบตัด New Kosoku",349,99,"ขนาด4x4"),
-("02","MAKITA สว่าน DF001G Cordless Driver Drill",3500,99,"40V"),
-("03","เครื่องตัดเหล็ก 12 Stanley",1400,99,"#15-166"),
-("04","ใบตัด Corolla",500,99,"ขนาด4x1"),
-("05","MAKITA สว่านไร้สาย สว่าน 3 ระบบ",849,99,"148V"),
-("06","Total ปั๊มลมอัตโนมัติ ปั๊มลมมินิ",1169,99,"12V 18A แรงดัน 120 PSI");
+("01","ใบตัด New Kosoku","New Kosoku","เครื่องมือตัด",349,99,"ขนาด4x4"),
+("02","MAKITA สว่าน DF001G Cordless Driver Drill","MAKITA","เครื่องมือไฟฟ้า",3500,99,"40V"),
+("03","เครื่องตัดเหล็ก 12 Stanley","Stanley","เครื่องมือตัด",1400,99,"#15-166"),
+("04","ใบตัด Corolla","Corolla","เครื่องมือตัด",500,99,"ขนาด4x1"),
+("05","MAKITA สว่านไร้สาย สว่าน 3 ระบบ","MAKITA","เครื่องมือลมปั้มลม",849,99,"148V"),
+("06","Total ปั๊มลมอัตโนมัติ ปั๊มลมมินิ","Total","เครื่องมือไฟฟ้า",1169,99,"12V 18A แรงดัน 120 PSI");
 
 SELECT * FROM USERS;
 
