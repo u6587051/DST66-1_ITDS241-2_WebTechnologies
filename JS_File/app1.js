021431419269fd652526940b83ac93ffbc8c8fa4
chocody <108443531+chocody@users.noreply.github.com>
2023-11-14 12:49:06 +0700

    edit path and create server backend

--- JS_File/app1.js
+++ JS_File/app1.js
@@ -165,8 +165,15 @@
 
 
 const express =require('express');
-const mysql = require('mysql2');
 const app = express();
+
+const AdminServiceRouter = require("./backend_WS/adminWS");
+const UserServiceRouter = require("./backend_WS/userWS");
+const ProductServiceRouter = require("./backend_WS/productWS");
+
+app.use("/adminWS",AdminServiceRouter);
+app.use("/userWS", UserServiceRouter);
+app.use("/productWS", ProductServiceRouter);
 
 app.listen(process.env.MYSQL_PORT, ()=>{
     console.log(`Server is listening to Port: ${process.env.MYSQL_PORT}`);
