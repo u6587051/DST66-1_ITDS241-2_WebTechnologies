d436c002288223adcfbde5938e8ebfe22406f038
thanaguide <thanawich.jue@student.mahidol.edu>
2023-11-14 12:53:21 +0700

    Update index.js

--- JS_Frontend/index.js
+++ JS_Frontend/index.js
@@ -6,6 +6,7 @@ const clientServerRouter = require(`${__dirname}/ClientServer`);
 
 app.use("/", clientServerRouter);
 
+
 // Server running on the port: 8021
 app.listen(port, function () {
   console.log(`Server listening at Port ${port}`);
