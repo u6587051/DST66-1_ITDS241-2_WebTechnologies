20588d08ef330be29e66ee170853892e83daacaa
chocody <108443531+chocody@users.noreply.github.com>
2023-11-14 12:42:27 +0700

    add router backend

---
+++ JS_File/productWS.js
@@ -0,0 +1,78 @@
+const express =require('express');
+const mysql = require('mysql2');
+const app = express();
+const dotenv = require('dotenv')
+dotenv.config();
+const router = express.Router();
+app.use(router);
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+let connection = mysql.createConnection({
+    host : process.env.MYSQL_HOST,
+    user : process.env.MYSQL_USERNAME,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE
+});
+
+router.get("/", function (req, res) {
+    return res.send({ message: "you are in product page" });
+});
+
+//แสดงหลังจากกดปุ่ม product
+router.get("/products", function (req, res) {
+    dbConn.query("SELECT * FROM product", function (error, results) {
+      if (error)
+        throw (error)
+      return res.send({ error: false, data: results, message: "Product lists" });
+    });
+  });
+
+router.post("/product", function (req, res) {
+    let product = req.body
+ 
+    connection.query(
+      "INSERT INTO product SET ? ",product,function (error, results) {
+        if (error)
+          throw(error);
+        return res.send({
+          error: false,
+          data: results.affectedRows,
+          message: "New product has been added.",
+        });
+      }
+    );
+  });
+
+router.put("/product", function (req, res) {
+    let product_id = req.body.PID;
+    let product = req.body;
+
+    connection.query("UPDATE product SET ? WHERE PID = ?",[product, product_id],function (error, results) {
+        if (error)
+          throw(error)
+        return res.send({
+          error: false,
+          data: results.affectedRows,
+          message: "Product has been updated.",
+        });
+      }
+    );
+});
+
+router.delete("/prodcut", function (req, res) {
+  let product_id = req.body.PID;
+
+  connection.query("DELETE FROM product WHERE PID = ?",[product_id],function (error, results) {
+      if (error)
+        throw(error)
+      return res.send({
+        error: false,
+        data: results.affectedRows,
+        message: "Product has been deleted.",
+      });
+    }
+  );
+});
+
+module.exports = router;
