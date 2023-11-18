3dfe7ae707f397b0752499dfba606ed1bcd06eb3
thanaguide <thanawich.jue@student.mahidol.edu>
2023-11-18 12:45:21 +0700

    Update index.js

--- JS_Frontend/index.js
+++ JS_Frontend/index.js
@@ -5,10 +5,12 @@ const path = require("path");
 
 const clientServerRouter = require(`${__dirname}/ClientServer`);
 
+// เรียกใช้งาน static file เช่น ไฟล์รูปภาพ ไฟล์ js และไฟล์ css
 app.use("/CallWs",express.static(path.join(__dirname,"CallWs")));
 app.use("/css",express.static(path.join(__dirname,"HTML/CSS")));
 app.use("/picture",express.static(path.join(__dirname,"picture")));
 
+// เรียกใช้งาน Router จากไฟล์ ClientServerRouter
 app.use("/", clientServerRouter);
 
 // Server running on the port: 8021
