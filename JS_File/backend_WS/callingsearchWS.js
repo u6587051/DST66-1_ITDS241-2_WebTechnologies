ae62bb1175ba73d40dfc6346b1bbe7d625d29cc6
thanaguide <thanawich.jue@student.mahidol.edu>
2023-11-18 16:30:40 +0700

    Search

--- JS_File/backend_WS/callingsearchWS.js
+++ JS_File/backend_WS/callingsearchWS.js
@@ -3,23 +3,27 @@ const axios = require("axios").default;
 const express = require("express");
 const router = express.Router();
 
-let searchURL = "http://localhost:8022/searchWS/search"; //กำหนด URL ที่ใช้ searchWS
+// กำหนด URL สำหรับ searchWS
+let searchURL = "http://localhost:8022/searchWS/search";
 
+// ฟังก์ชันในการเรียกรับ search results แบบ asynchronous
 let getResults = async () => {
   try {
     let response = await axios.get(searchURL, { responseType: "json" });
     console.log(`Use Async/Await`);
     console.log(`The number of results: ${response.data.data.length}`);
     console.log(response.data);
-    return response.data;
+    return response.data; // Return the retrieved data
   } catch (error) {
     console.error(error);
     return { error: true };
   }
 };
 
+// กำหนด route สำหรับ handling GET requests
 router.get("/", async (req, res) => {
   res.json(await getResults());
 });
 
+// Export router
 module.exports = router;
