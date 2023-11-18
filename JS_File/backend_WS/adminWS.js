7e5bcb75f2608e6fe2e415e1247e5eafd48c20b5
u6587051 <jittakan.dam@student.mahidol.edu>
2023-11-18 23:56:52 +0700

    Update adminWS.js

--- JS_File/backend_WS/adminWS.js
+++ JS_File/backend_WS/adminWS.js
@@ -1,5 +1,5 @@
 //import express,mysql,router
-const express =require('express');
+const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const router = express.Router();
@@ -28,28 +28,28 @@ router.use(express.urlencoded({ extended: true }));
 
 //สร้างตัวเชื่อมต่อ database
 let connection = mysql.createConnection({
-    host : process.env.MYSQL_HOST,
-    user : process.env.MYSQL_USERNAME,
-    password : process.env.MYSQL_PASSWORD,
-    database : process.env.MYSQL_DATABASE
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 //รับ get มาแล้วแสดงข้อความว่าอยู่ในหน้าการทำงาน admin
 router.get("/", function (req, res) {
-    return res.send({ message: "you are in admin page" });
+  return res.send({ message: "you are in admin page" });
 });
 
 //รับ get มาแล้วแสดงผล admin ทั้งหมด
-router.get("/admins",function (req, res) {
-    connection.query("SELECT * FROM ADMINS", function (error, results) {
-      if (error)
-        throw (error)
-      return res.send({ error: false, data: results, message: "Admin lists" });
-    });
+router.get("/admins", function (req, res) {
+  connection.query("SELECT * FROM ADMINS", function (error, results) {
+    if (error)
+      throw (error)
+    return res.send({ error: false, data: results, message: "Admin lists" });
+  });
 });
 
 //รับค่า get มาแล้วรับค่าไอดี params เพื่อแสดงผล admin ที่มีไอดีที่กำหนด
-router.get("/admin",function (req, res) {
+router.get("/admin", function (req, res) {
   let aid = req.query.AID;
   let aemail = req.query.EMAIL;
   let afname = req.query.FNAME;
@@ -63,50 +63,50 @@ router.get("/admin",function (req, res) {
             EMAIL LIKE "%${aemail}%" AND
             FNAME LIKE "%${afname}%";`
 
-  connection.query(sql,function (error, results) {
-      if (error || results.length === 0)
-        return res.send({
-          error: true,
-          message: "Admin is not found.",
-        });
+  connection.query(sql, function (error, results) {
+    if (error || results.length === 0)
       return res.send({
-        error: false,
-        data: results,
-        message: "Admin retrieved",
+        error: true,
+        message: "Admin is not found.",
       });
+    return res.send({
+      error: false,
+      data: results,
+      message: "Admin retrieved",
+    });
   });
 });
 
 //รับ put มาเพื่ออัพเดทข้อมูลใน database จาก admin id และอัพเดทข้อมูลจากข้อมูลที่ได้รับ
-router.put("/admin",function (req, res) {
-    let admin_id = req.body.AID;
-    let admin = req.body;
+router.put("/admin", function (req, res) {
+  let admin_id = req.body.AID;
+  let admin = req.body;
 
-    connection.query("UPDATE ADMINS SET ? WHERE AID = ?",[admin, admin_id],function (error, results) {
-        if (error)
-          throw(error)
-        return res.send({
-          error: false,
-          data: results.affectedRows,
-          message: "Admin has been updated.",
-        });
-      }
-    );
+  connection.query("UPDATE ADMINS SET ? WHERE AID = ?", [admin, admin_id], function (error, results) {
+    if (error)
+      throw (error)
+    return res.send({
+      error: false,
+      data: results.affectedRows,
+      message: "Admin has been updated.",
+    });
+  }
+  );
 });
 
 //รับ delete มาเพื่อลบข้อมูล admin จาก admin id ที่กำหนด
-router.delete("/admin",function (req, res) {
+router.delete("/admin", function (req, res) {
   let admin_id = req.body.AID;
 
-  connection.query("DELETE FROM ADMINS WHERE AID = ?",[admin_id],function (error, results) {
-      if (error)
-        throw(error)
-      return res.send({
-        error: false,
-        data: results.affectedRows,
-        message: "Admin has been deleted.",
-      });
-    }
+  connection.query("DELETE FROM ADMINS WHERE AID = ?", [admin_id], function (error, results) {
+    if (error)
+      throw (error)
+    return res.send({
+      error: false,
+      data: results.affectedRows,
+      message: "Admin has been deleted.",
+    });
+  }
   );
 });
 
