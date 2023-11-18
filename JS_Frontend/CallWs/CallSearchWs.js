77e5d3f07b0c1ec8b5fa24e91a48e8d519dcd99f
thanaguide <thanawich.jue@student.mahidol.edu>
2023-11-19 01:27:41 +0700

    กหฟกฟ

--- JS_Frontend/CallWs/CallSearchWs.js
+++ JS_Frontend/CallWs/CallSearchWs.js
@@ -1,13 +1,13 @@
 //function ในการเรียก searchWS ผ่านการ fetch api
-async function callSearchWS(url, method, sentData = {}) {
-  let data;
-  if (method == "select") { //ถ้า method ที่รับ parameter คือแสดงผลทั้งหมด
-    let response = await fetch(url, {
-      method: "GET", //ส่ง method get ไปยัง SearchWS
-    });
-    data = await response.json();
-  }
-  return data; //ส่งข้อมูลกลับมา
+async function callSearchWS(url, method,sentData = {}) {
+    let data;
+    if (method == "select") { //ถ้า method ที่รับ parameter คือแสดงผลทั้งหมด
+      let response = await fetch(url, {
+        method: "GET", //ส่ง method get ไปยัง SearchWS
+      });
+      data = await response.json();
+    }
+    return data; //ส่งข้อมูลกลับมา
 }
 
 //รับค่า input box โดย id
@@ -20,67 +20,99 @@ let PBRAND_TXT = document.querySelector("#pbrand");
 let selectB = document.querySelector("#psearch");
 
 function clearInput() { //function ในการ clear กล่อง input box
-  PID_TXT.value = "";
   PNAME_TXT.value = "";
   PBRAND_TXT.value = "";
   PCAT_TXT.value = "";
-  PPRICE_TXT.value = "";
-  PQUAN_TXT.value = "";
-  PDETAIL_TXT.value = "";
-  PIMG_TXT.value = "";
 }
 
 
 //หากกดคลิก select button ให้รับค่ามาเก็บเป็น json ไฟล์แล้วส่งเข้าไปพร้อมเรียก function callSearchWS
 //ส่ง parameter url คือ http://localhost:8022/searchWS/search บวกกับ params เพื่อใช้ในการใส่เงื่อนไขการค้นหา
 selectB.addEventListener("click", () => {
-  PBRAND = PBRAND_TXT.value;
-  PCAT = PCAT_TXT.value;
-  PNAME = PNAME_TXT.value;
+    PBRAND = PBRAND_TXT.value;
+    PCAT = PCAT_TXT.value;
+    PNAME = PNAME_TXT.value;
 
-  let pname = `PNAME=${PNAME}`
-  let cat = `PCAT=${PCAT}`;
-  let brand = `PBRAND=${PBRAND}`;
+    let pname = `PNAME=${PNAME}`
+    let cat = `PCAT=${PCAT}`;
+    let brand = `PBRAND=${PBRAND}`;
 
-  callSearchWS("http://localhost:8022/searchWS/" + "search?" + pname + "&" + cat + "&" + brand, "select").then((data) => {
-    console.log(data);
-    if (data) {
-      alert(data.message);
-      data.data.forEach((productData) => {
-        var column = document.createElement('div');
-        column.className = 'column';
+    callSearchWS("http://localhost:8022/searchWS/" + "search?"+pname+"&"+cat+"&"+brand, "select").then((data) => {
+      console.log(data);
+      if (data.data.length > 0) {
+        alert(data.message);
+        $("#Soutput").empty();
+        data.data.forEach((productData) => {
+          var column = document.createElement('div'); 
+          column.className = 'column';
 
-        var img = document.createElement('img');
-        img.src = productData.PIMG; // Replace with the actual property name from your data
-        // img.alt = productData.altText;
-        img.className = 'productimg';
+          var img = document.createElement('img');
+          img.src = productData.Pimg; // Replace with the actual property name from your data
+          // img.alt = productData.altText;
+          img.className = 'productimg';
+          console.log(productData)
 
-        var heading = document.createElement('h3');
-        heading.id = 'producthead';
-        heading.textContent = productData.PNAME; // Replace with the actual property name from your data
+          var heading = document.createElement('h3');
+          heading.id = 'producthead';
+          heading.textContent = productData.PName; // Replace with the actual property name from your data
+          var price = document.createElement('h4');
+          price.id = 'price';
+          price.textContent = '฿' + productData.PPrice; // Replace with the actual property name from your data
 
-        var price = document.createElement('h4');
-        price.id = 'price';
-        price.textContent = '฿' + productData.PPRICE; // Replace with the actual property name from your data
+          var buyButton = document.createElement('h4');
+          buyButton.id = 'buybutton';
+          
+          var link = document.createElement('a');
+          link.id = 'Buy';
+          // link.href = productData.buyLink; // Replace with the actual property name from your data
+          link.textContent = 'สั่งซื้อสินค้า';
 
-        var buyButton = document.createElement('h4');
-        buyButton.id = 'buybutton';
+          buyButton.appendChild(link);
 
-        var link = document.createElement('a');
-        link.id = 'Buy';
-        // link.href = productData.buyLink; // Replace with the actual property name from your data
-        link.textContent = 'สั่งซื้อสินค้า';
+          column.appendChild(img);
+          column.appendChild(heading);
+          column.appendChild(price);
+          column.appendChild(buyButton);
 
-        buyButton.appendChild(link);
+          $("#Soutput").append(column); // Assuming .content is a valid target
+          clearInput();
+          });
 
-        // column.appendChild(img);
-        column.appendChild(heading);
-        column.appendChild(price);
-        column.appendChild(buyButton);
-
-        $("#Soutput").html(column);
-        clearInput();
-      })
-    }
-  })
+    
+      }
+  });
 });
+// function createProductElement(productData) {
+//     var column = document.createElement('div');
+//     column.className = 'column';
+
+//     var img = document.createElement('img');
+//     img.src = productData.PIMG; // Replace with the actual property name from your data
+//     // img.alt = productData.altText;
+//     img.className = 'productimg';
+
+//     var heading = document.createElement('h3');
+//     heading.id = 'producthead';
+//     heading.textContent = productData.PNAME; // Replace with the actual property name from your data
+
+//     var price = document.createElement('h4');
+//     price.id = 'price';
+//     price.textContent = '฿' + productData.PPRICE; // Replace with the actual property name from your data
+
+//     var buyButton = document.createElement('h4');
+//     buyButton.id = 'buybutton';
+    
+//     var link = document.createElement('a');
+//     link.id = 'Buy';
+//     // link.href = productData.buyLink; // Replace with the actual property name from your data
+//     link.textContent = 'สั่งซื้อสินค้า';
+
+//     buyButton.appendChild(link);
+
+//     column.appendChild(img);
+//     column.appendChild(heading);
+//     column.appendChild(price);
+//     column.appendChild(buyButton);
+
+//    document.getElementById("Soutput").innerHTML = column
+// }
