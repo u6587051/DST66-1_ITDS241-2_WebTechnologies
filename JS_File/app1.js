472a4b88a0fbbb122662c70695ac0bf6f4b55037
thanaguide <thanawich.jue@student.mahidol.edu>
2023-11-19 23:08:23 +0700

    Revert "path fixed"
    This reverts commit 4c9591c9cfc667d26724ed646fcfaee3010ce051.

--- JS_File/app1.js
+++ JS_File/app1.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 
-const AdminServiceRouter = require("./sec1_gr1_ws_src/adminWS");
-const ProductServiceRouter = require("./sec1_gr1_ws_src/productWS");
-const SearchServiceRouter = require("./sec1_gr1_ws_src/searchWS");
-const LoginServiceRouter = require("./sec1_gr1_ws_src/loginWS");
-const CallingAdminServiceRouter = require("./sec1_gr1_ws_src/callingadminWS");
-const CallingProductServiceRouter = require("./sec1_gr1_ws_src/callingproductWS");
-const CallingSearchWS = require("./sec1_gr1_ws_src/callingsearchWS");
-const CallingLoginWS = require("./sec1_gr1_ws_src/callingloginWS");
+const AdminServiceRouter = require("./backend_WS/adminWS");
+const ProductServiceRouter = require("./backend_WS/productWS");
+const SearchServiceRouter = require("./backend_WS/searchWS");
+const LoginServiceRouter = require("./backend_WS/loginWS");
+const CallingAdminServiceRouter = require("./backend_WS/callingadminWS");
+const CallingProductServiceRouter = require("./backend_WS/callingproductWS");
+const CallingSearchWS = require("./backend_WS/callingsearchWS");
+const CallingLoginWS = require("./backend_WS/callingloginWS");
 
 app.use("/adminWS",AdminServiceRouter);
 app.use("/productWS", ProductServiceRouter);
